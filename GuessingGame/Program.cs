9a2bcb69656f5f2ab05deb6973b71a5664a394c7
Bernard828 <bernard.lackey1@gmail.com>
2020-05-14 13:00:15 -0400

    loaded 3

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -7,23 +7,44 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Guessing Game!");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(" Enter a guess.");
+                string userGuess = Console.ReadLine();
+                if (userGuess == "7")
+                {
+                    Console.WriteLine("You guessed right!");
 
-            Console.WriteLine(" Enter a guess.");
-            string userGuess = Console.ReadLine();
-            if (userGuess == "7")
-            {
-                Console.WriteLine("You guessed right!");
+                }
+                else if (userGuess == "0")
+                {
+                    Console.WriteLine("Enter an number between 1-10.");
+                }
+                else
+                {
+                    Console.WriteLine("You Lose!");
+                }
+            }
+           
 
-            }
-            else if (userGuess == "0")
-            {
-                Console.WriteLine("Enter an number between 1-10.");
-            }
-            else
-            {
-                Console.WriteLine("You Lose!");
-            }
-            //User puts in 0, provide instrucstions
+            //Console.WriteLine("Try a second guess");
+            //string secondGuess = Console.ReadLine();
+            //int i = Convert.ToInt32(secondGuess);
+            //number2 = Convert.ToBoolean(i);
+
+            //if (secondGuess == "7")
+            //{
+            //    Console.WriteLine("Great job!");
+            //}
+            //else if (0 < secondGuess < 11; != "7")
+            //{
+            //    Console.WriteLine("Sorry, You Lose!");
+            //}
+           
+            // ( i = ""; 0 < i < 11)
+            //{
+            //   Console.WriteLine() ;
+            //}
 
         }
 
