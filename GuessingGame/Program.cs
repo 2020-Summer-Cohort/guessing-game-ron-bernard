7817553ff02135e7ddd57bb5991843abd63f2a77
Bernard828 <bernard.lackey1@gmail.com>
2020-05-14 11:48:49 -0400

    module one finished

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -7,6 +7,19 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Guessing Game!");
+
+            Console.WriteLine(" Enter a guess.");
+            string userGuess = Console.ReadLine();
+            if (userGuess == "7")
+            {
+                Console.WriteLine("You guessed right!");
+
+            }
+            else
+            {
+                Console.WriteLine("You Lose!");
+            }
         }
+
     }
 }
