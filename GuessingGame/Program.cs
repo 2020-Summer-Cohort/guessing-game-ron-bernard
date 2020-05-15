17e3f507a052c57f853365618a76210ceab1ddf4
roncwynn <rcwynn@gmail.com>
2020-05-15 10:01:14 -0400

    stretch tasks finished

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -13,39 +13,45 @@ namespace GuessingGame
             Random random = new Random();
             int secretNumber = random.Next(1, 10);
 
-            Console.WriteLine(secretNumber);
-            while (winningGuess == false & guessTrys < 2)
+            //Console.WriteLine(secretNumber);
+
+            while (winningGuess == false & guessTrys < 3)
             {
-                Console.WriteLine(" Enter a guess.");
+                Console.WriteLine("\nEnter a guess.");
                 int userGuess = Convert.ToInt32(Console.ReadLine());
 
                 if (userGuess == -1)
                 {
-                    Console.WriteLine("GAME EXITED by user.");
+                    Console.WriteLine("\nGAME EXITED by user.");
                     guessTrys = 2;
                 }
                 else if (userGuess == 0)
                 {
-                    Console.WriteLine("Enter an number between 1-10.");
+                    Console.WriteLine("\nEnter an number between 1-10.");
                 }
                 else if (userGuess == secretNumber)
                 {
-                    Console.WriteLine("You guessed right!");
+                    Console.WriteLine("\nYou guessed right!");
                     winningGuess = true;
                 }
                 else
                 {
-                    Console.WriteLine("You Lose!");
+                    Console.WriteLine("\nTry Again!");
                     if (userGuess > secretNumber)
                     {
-                        Console.WriteLine("You guessed to high");
+                        Console.WriteLine("\nYou guessed to high");
                     }
                     else
                     {
-                        Console.WriteLine("You guessed to low");
+                        Console.WriteLine("\nYou guessed to low");
                     }
                 }
                 guessTrys = guessTrys + 1;
+            }
+        
+            if (!winningGuess)
+            {
+                Console.WriteLine("\nYou Lose");
             }
         }
 
